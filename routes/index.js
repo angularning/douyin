5fe03a98654ca5f03611f81403aff18f5510719f
449125406@qq.com <nlj13141314>
2019-03-26 09:55:16 +0800

    1111

--- routes/index.js
+++ routes/index.js
@@ -9,29 +9,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-var https = require("https");
-var iconv = require("iconv-lite");
-var url="https://api.weixin.qq.com/sns/oauth2/access_token?appid="+appid+"&secret="+secret+"&code="+code+"&grant_type=authorization_code";
-https.get(url, function (res) {
-    var datas = [];
-    var size = 0;
-    res.on('data', function (data) {
-        datas.push(data);
-        size += data.length;
-        //process.stdout.write(data);
-    });
-    res.on("end", function () {
-        var buff = Buffer.concat(datas, size);
-        var result = iconv.decode(buff, "utf8");//转码//var result = buff.toString();//不需要转编码,直接tostring
-        console.log(result);
-    });
-}).on("error", function (err) {
-    Logger.error(err.stack)
-    callback.apply(null);
-});
-
-
-
 router.get('/test', function(req, res, next) {
     var r = generateRandom().toString();
     var url=req.query.url;
